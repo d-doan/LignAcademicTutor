7b65d5681c08e329d9ad94b8e83052a99a36e8b5
d-doan <44249543+d-doan@users.noreply.github.com>
2023-11-30 12:12:08 -0800

    session-based question history

--- server/main/main.py
+++ server/main/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, request, url_for, flash
+from flask import Blueprint, render_template, redirect, request, session, url_for, flash
 from server.models.models import RegistrationCode, User, db
 from flask_login import current_user, login_user, logout_user
 
@@ -9,3 +9,28 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('home.html')
+
+
+@main.route('/question')
+def question():
+    return render_template('question.html')
+
+@main.route('/choose/<choice>')
+def choose(choice):
+    if 'choices' not in session:
+        session['choices'] = []
+
+    session['choices'].append(choice)
+    session.modified = True  # Mark the session as modified
+    return redirect(url_for('main.show_choices'))
+
+@main.route('/choices')
+def show_choices():
+    choices = session.get('choices', [])
+    return render_template('choices.html', choices=reversed(choices))
+
+@main.route('/reset')
+def reset():
+    session.pop('choices', None)  # Remove 'choices' from session
+    return redirect(url_for('main.question'))
+
